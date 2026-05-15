740cbefc0cb687570726330d3a8ebc035f030e00
Mantas <57765655+mantaszm@users.noreply.github.com>
2026-05-15 18:01:26 +0300

    Refactor Vector class with enhanced documentation

--- vector/mano_vector.h
+++ vector/mano_vector.h
@@ -3,9 +3,12 @@
 #include <cstddef>
 #include <algorithm>
 #include <initializer_list>
-#include <stdexcept>
 #include <utility>
 
+/**
+ * @brief Dinaminis masyvas (std::vector analogas)
+ * @tparam T elementų tipas
+ */
 template <typename T>
 class Vector {
 private:
@@ -14,12 +17,14 @@ private:
     size_t capacity_;
     size_t reallocations_;
 
+    /**
+     * @brief Vidinis atminties didinimas
+     */
     void reallocate(size_t new_capacity) {
         T* new_data = new T[new_capacity];
 
-        for (size_t i = 0; i < size_; ++i) {
+        for (size_t i = 0; i < size_; ++i)
             new_data[i] = std::move(data_[i]);
-        }
 
         delete[] data_;
         data_ = new_data;
@@ -32,16 +37,24 @@ public:
     using iterator = T*;
     using const_iterator = const T*;
 
-    // Constructors / Destructor
+    /**
+     * @brief Default konstruktorius
+     */
     Vector()
         : data_(nullptr), size_(0), capacity_(0), reallocations_(0) {}
 
+    /**
+     * @brief Sukuria vector su n elementų
+     */
     explicit Vector(size_t n, const T& value = T())
         : data_(new T[n]), size_(n), capacity_(n), reallocations_(0) {
         for (size_t i = 0; i < n; ++i)
             data_[i] = value;
     }
 
+    /**
+     * @brief initializer_list konstruktorius
+     */
     Vector(std::initializer_list<T> init)
         : Vector(init.size()) {
         size_t i = 0;
@@ -49,14 +62,16 @@ public:
             data_[i++] = v;
     }
 
+    /**
+     * @brief Destruktorius
+     */
     ~Vector() {
         delete[] data_;
-        size_ = 0;
-        capacity_ = 0;
-        reallocations_ = 0;
     }
 
-    // Copy
+    /**
+     * @brief Copy konstruktorius
+     */
     Vector(const Vector& other)
         : data_(new T[other.capacity_]),
           size_(other.size_),
@@ -66,6 +81,9 @@ public:
             data_[i] = other.data_[i];
     }
 
+    /**
+     * @brief Copy assignment
+     */
     Vector& operator=(const Vector& other) {
         if (this == &other) return *this;
 
@@ -79,10 +97,13 @@ public:
         data_ = new_data;
         size_ = other.size_;
         capacity_ = other.capacity_;
+
         return *this;
     }
 
-    //Move
+    /**
+     * @brief Move konstruktorius
+     */
     Vector(Vector&& other) noexcept
         : data_(other.data_),
           size_(other.size_),
@@ -95,6 +116,9 @@ public:
         other.reallocations_ = 0;
     }
 
+    /**
+     * @brief Move assignment
+     */
     Vector& operator=(Vector&& other) noexcept {
         if (this == &other) return *this;
 
@@ -108,19 +132,20 @@ public:
         other.data_ = nullptr;
         other.size_ = 0;
         other.capacity_ = 0;
-        other.reallocations_ = 0;
 
         return *this;
     }
 
-    // Element access
-    T& operator[](size_t i) {
-        return data_[i];
-    }
+    // =========================
+    // ELEMENT ACCESS
+    // =========================
 
-    const T& operator[](size_t i) const {
-        return data_[i];
-    }
+    /**
+     * @brief operator []
+     */
+    T& operator[](size_t i) { return data_[i]; }
+
+    const T& operator[](size_t i) const { return data_[i]; }
 
     T& front() { return data_[0]; }
     T& back() { return data_[size_ - 1]; }
@@ -128,7 +153,10 @@ public:
     const T& front() const { return data_[0]; }
     const T& back() const { return data_[size_ - 1]; }
 
-    // Capacity
+    // =========================
+    // CAPACITY
+    // =========================
+
     size_t size() const noexcept { return size_; }
     size_t capacity() const noexcept { return capacity_; }
     bool empty() const noexcept { return size_ == 0; }
@@ -136,17 +164,22 @@ public:
     size_t getReallocations() const noexcept { return reallocations_; }
 
     void reserve(size_t new_cap) {
-        if (new_cap <= capacity_) return;
-        reallocate(new_cap);
+        if (new_cap > capacity_)
+            reallocate(new_cap);
     }
 
     void shrink_to_fit() {
-        if (size_ < capacity_) {
+        if (size_ < capacity_)
             reallocate(size_);
-        }
     }
 
-    // Modifiers
+    // =========================
+    // MODIFIERS
+    // =========================
+
+    /**
+     * @brief push_back
+     */
     void push_back(const T& value) {
         if (size_ == capacity_) {
             size_t new_cap = (capacity_ == 0) ? 1 : capacity_ * 2;
@@ -163,14 +196,24 @@ public:
         data_[size_++] = std::move(value);
     }
 
+    /**
+     * @brief pop_back
+     */
     void pop_back() {
-        if (size_ > 0) --size_;
+        if (size_ > 0)
+            --size_;
     }
 
+    /**
+     * @brief clear
+     */
     void clear() {
         size_ = 0;
     }
 
+    /**
+     * @brief erase pagal index
+     */
     void erase(size_t index) {
         if (index >= size_) return;
 
@@ -180,33 +223,38 @@ public:
         --size_;
     }
 
-    iterator erase(iterator first, iterator last) {
-        size_t start = first - begin();
-        size_t endPos = last - begin();
-
-        size_t newSize = size_ - (endPos - start);
-
-        for (size_t i = start; i < newSize; ++i) {
-            data_[i] = std::move(data_[i + (endPos - start)]);
-        }
-
-        size_ = newSize;
-
-        return begin() + start;
-    }
-
+    /**
+     * @brief erase iterator (vienas elementas)
+     */
     iterator erase(iterator it) {
-    size_t index = it - data_;
+        size_t index = it - data_;
 
-    for (size_t i = index; i < size_ - 1; ++i) {
-        data_[i] = std::move(data_[i + 1]);
+        for (size_t i = index; i < size_ - 1; ++i)
+            data_[i] = std::move(data_[i + 1]);
+
+        --size_;
+        return data_ + index;
     }
 
-    --size_;
+    /**
+     * @brief erase intervalas
+     */
+    iterator erase(iterator first, iterator last) {
+        size_t start = first - data_;
+        size_t endPos = last - data_;
 
-    return data_ + index;
-}
+        size_t shift = endPos - start;
 
+        for (size_t i = start; i < size_ - shift; ++i)
+            data_[i] = std::move(data_[i + shift]);
+
+        size_ -= shift;
+        return data_ + start;
+    }
+
+    /**
+     * @brief insert
+     */
     void insert(size_t index, const T& value) {
         if (index > size_) return;
 
@@ -222,14 +270,20 @@ public:
         ++size_;
     }
 
-    // Iterators
+    // =========================
+    // ITERATORS
+    // =========================
+
     iterator begin() { return data_; }
     iterator end() { return data_ + size_; }
 
     const_iterator begin() const { return data_; }
     const_iterator end() const { return data_ + size_; }
 
-    // Operators
+    // =========================
+    // OPERATORS
+    // =========================
+
     bool operator==(const Vector& other) const {
         if (size_ != other.size_) return false;
 
@@ -244,7 +298,13 @@ public:
         return !(*this == other);
     }
 
-    // Utility
+    // =========================
+    // UTILITY
+    // =========================
+
+    /**
+     * @brief swap
+     */
     void swap(Vector& other) noexcept {
         std::swap(data_, other.data_);
         std::swap(size_, other.size_);
